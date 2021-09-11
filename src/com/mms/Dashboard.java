1b51fd82a28f9a014eae0394c16ef5cda2335518
Rana0001 <Magarprashant001@gmail.com>
2021-09-11 23:37:10 +0545

    Fifth Commit

--- src/com/mms/Dashboard.java
+++ src/com/mms/Dashboard.java
@@ -6,24 +6,33 @@ import java.awt.*;
 
 public class Dashboard {
     JFrame dashboard;
-    JLabel label,banner_label;
+    JLabel label,banner_label,txt_title;
     JButton btn;
     JPanel leftpanel,mainpanel;
     JOptionPane msg;
+    Font font;
     public Dashboard(){
         dashboard = new JFrame("DashBoard");
+        font = new Font("Cambria", Font.ITALIC, 35);
 
         leftpanel = new JPanel();
         leftpanel.setBorder(BorderFactory.createMatteBorder(8,8,8,8,Color.decode("#1A2B63")));
         leftpanel.setBounds(1,0,300,722);
         dashboard.add(leftpanel);
 
+        txt_title = new JLabel("Welcome To Student Module");
+        txt_title.setFont(font);
+        txt_title.setForeground(Color.decode("#E9EDF5"));
+        txt_title.setBounds(460,50,450,120);
+        dashboard.add(txt_title);
+
         // Add Banner Image in DashBoard
         ImageIcon bannerIcon = new ImageIcon(new ImageIcon("src\\icons\\banner.png").getImage().getScaledInstance(700, 300, Image.SCALE_SMOOTH));
-        JLabel banner_label = new JLabel(bannerIcon);
+        banner_label = new JLabel(bannerIcon);
         banner_label.setIcon(bannerIcon);
         banner_label.setBounds(320,20,700,300);
         dashboard.add(banner_label);
+
 
 
 
