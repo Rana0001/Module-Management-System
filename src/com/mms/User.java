77801082b81c00bbed8b2476a2e3499be349b123
Rana0001 <Magarprashant001@gmail.com>
2021-09-12 15:33:28 +0545

    Seventh Commit

---
+++ src/com/mms/User.java
@@ -0,0 +1,58 @@
+package com.mms;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String contact;
+    private String password;
+    private String check;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCheck() {
+        return check;
+    }
+
+    public void setCheck(String check) {
+        this.check = check;
+    }
+}
