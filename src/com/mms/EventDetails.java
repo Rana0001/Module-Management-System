8536f0842384793869b4b106236c38569b978f82
Rana0001 <Magarprashant001@gmail.com>
2021-09-13 21:55:08 +0545

    Nine Commit

--- src/com/mms/EventDetails.java
+++ src/com/mms/EventDetails.java
@@ -7,17 +7,22 @@ public class EventDetails {
     String event_name;
     String event_date;
     String event_notice;
-    public EventDetails(String event_id,String event_name,String event_date,String event_notice){
+
+    public EventDetails(String event_id, String event_name, String event_date, String event_notice) {
         this.event_id = event_id;
         this.event_name = event_name;
         this.event_date = event_date;
         this.event_notice = event_notice;
+        try {
+            Database db = new Database();
+            String query = "Insert into event(event_id,event_name,event_date,event_notice) values('" + this.event_id + "','" + this.event_name + "','" + this.event_date + "','" + this.event_notice + "')";
+            int ans = db.insert(query);
+            if (ans > 0) {
+                JOptionPane.showMessageDialog(null, "Data Inserted Sucessfully..");
+            }
 
-        Database db = new Database();
-        String query = "Insert into event(event_id,event_name,event_date,event_notice) values('"+this.event_id+"','"+this.event_name+"','"+this.event_date+"','"+this.event_notice+"')";
-        int ans = db.insert(query);
-        if (ans>0){
-            JOptionPane.showMessageDialog(null,"Data Inserted Sucessfully..");
+        }catch (Exception e){
+            e.printStackTrace();
         }
-
-    }}
+    }
+}
